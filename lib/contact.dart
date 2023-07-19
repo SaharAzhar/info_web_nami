0033f03c1bcecf6755a225f241e12b1ceb385f28
Sahar Azhar <saharsheikh229@gmail.com>
2023-07-19 16:05:15 +0500

    new code files

---
+++ lib/contact.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class ContatcScr extends StatefulWidget {
+  const ContatcScr({super.key});
+
+  @override
+  State<ContatcScr> createState() => _ContatcScrState();
+}
+
+class _ContatcScrState extends State<ContatcScr> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
+              children: [],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
